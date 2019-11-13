e8dde83c42527d1b79c42f361402e01a4897f8d9
Justin Gomez <justingomez@JDG-MacBook-Pro-8.local>
2019-11-12 23:42:14 -0800

    Tests and measures in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#include <algorithm>
+#include <chrono>
+#include <ctime>
 #include <iostream>
-#include "MyTree.h"
 using namespace std;
+using namespace std::chrono;
+#include "MyTree.h"
+
+string randstring();
 
 int main(){
   char userChar = ' ';
@@ -11,6 +17,7 @@ int main(){
          << "Enter p to print in PreOrder" << endl
          << "Enter f to find the max value" << endl
          << "Enter c to convert Binary Tree to BST" << endl
+         << "Enter t to test and measure performance" << endl
          << "Enter q to quit" << endl;
     cin >> userChar;
     cout << endl;
@@ -46,6 +53,71 @@ int main(){
 
       userTree.makeBST();
     }
+    else if(userChar == 't'){
+      cout << "MEASURE PERFORMACE" << endl;
+
+      double compTimes[4][4] = {0};
+      MyTree testTree;
+      int new_size = 100;
+
+      // test 4 sizes (100, 1000, 10000, 100000)
+      for(unsigned k = 0; k < 4; ++k){
+
+        // populate curr
+        nodePair *curr = new nodePair[new_size];
+        for(unsigned j = 0; j < new_size; ++j){
+          nodePair a;
+          a.first = rand();
+          a.second = randstring();
+          curr[j] = a;
+        }
+
+        duration<double> runTime;
+        for(unsigned i = 0; i < 4; ++i){
+          auto start = high_resolution_clock::now();
+
+          if(i == 0){
+            for(unsigned j = 0; j < new_size; ++j){
+              testTree.insert(curr[j].first, curr[j].second);
+            }
+          }
+          else if(i == 1){
+            testTree.preorder();
+          }
+          else if(i == 2){
+            testTree.findMax();
+          }
+          else{
+            testTree.makeBST();
+          }
+          auto finish = high_resolution_clock::now();
+          compTimes[k][i] += duration_cast<microseconds>(finish - start).count();
+        }
+        new_size *= 10;
+      }
+
+      cout << endl << "RESULTS: (µs)" << endl << endl;
+      cout << "Sizes         ||  100 ||  1000  || 10000  ||  100000  ||" << endl;
+      for(unsigned i = 0; i < 4; ++i){
+        if(i == 0){
+          cout << "insert(*,*)   ||  ";
+        }
+        else if(i == 1){
+          cout << "preorder()    ||  ";
+        }
+        else if(i == 2){
+          cout << "findMax()     ||  ";
+        }
+        else{
+          cout << "makeBST()     ||  ";
+        }
+        for(unsigned j = 0; j < 4; ++j){
+          cout << compTimes[j][i] << "  ||  " ;
+        }
+        cout << endl;
+      }
+
+    }
     else if(userChar != 'q'){
       cout << "Invald command." << endl;
     }
@@ -53,4 +125,14 @@ int main(){
   }
 
   return 0;
+}
+
+string randstring(){
+  string a;
+  // All strings will have length 10
+  for(unsigned i = 0; i < 10; ++i){
+    char b = 97 + (rand() % 26);
+    a += b;
+  }
+  return a;
 }
